6a9d3e1973ded4d7517d18d9aad62841b27f4d3f
suresh <suresh.avlm@gmail.com>
2018-03-18 08:19:22 +0530

    app.js port number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 8080;
+var port = "8080";//process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
 
